bc067470365aec78bee7cbf90998870a9fb6890c
nxs-clarissa <nxs.clarissa@gmail.com>
2024-11-20 11:17:46 +0700

    first commit

---
+++ src/data/todos.ts
@@ -0,0 +1,19 @@
+import { Todo } from "../types/todo";
+
+export const dummyData: Todo[] = [
+    {
+        id: 1,
+        title: "Like this shimkongz",
+        completed: false,
+    },
+    {
+        id: 2,
+        title: "Subscribe yourself",
+        completed: false,
+    },
+    {
+        id: 3,
+        title: "Leave your trace",
+        completed: false,
+    },
+];
